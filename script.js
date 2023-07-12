f8dd9df83af5c74a9008c30c1cc005a747ea45b5
NickKumar <Timerbob@gmail.com>
2023-07-11 19:32:01 -0600

    -

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 const canvas = document.getElementById("canvas");
 console.log(canvas);
+const ctx = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+addEventListener("resize", () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+
+  init();
+});
+
+function init() {
+  console.log("Bork!");
+}
