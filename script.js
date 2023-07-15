530dcaf1a82c4935ada5a7301e53f50d75d8a0da
NickKumar <Timerbob@gmail.com>
2023-07-15 15:55:16 -0600

    On-click stars designed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////
 // Utility Functions
 
-const randomColor = (min, max) =>
+const randomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
 let centerX = window.innerWidth / 2;
@@ -50,12 +50,12 @@ class backgroundStar {
     this.x = Math.random() * canvas1.width;
     this.y = Math.random() * canvas1.height;
     this.z = Math.random() + size;
-    this.dim = randomColor(0.5, 1);
-    this.direction = "up";
-    this.special = `rgb(${randomColor(150, 255)},${randomColor(
+    this.dim = randomInt(0.5, 1);
+    // this.direction = "up";
+    this.special = `rgb(${randomInt(150, 255)},${randomInt(
       150,
       200
-    )},${randomColor(200, 255)}`;
+    )},${randomInt(200, 255)}`;
   }
 
   // White Stars
@@ -77,12 +77,9 @@ class backgroundStar {
     ctx.closePath();
 
     // star body
-
     ctx.beginPath();
-
     ctx.fillStyle = this.special;
     ctx.arc(this.x, this.y, this.z + 2, 0, Math.PI * 2);
-
     ctx.fill();
     ctx.closePath();
   }
@@ -104,6 +101,8 @@ for (let i = 0; i < density; i++) {
   // console.log(density / 30);
   i < density / 30 ? starfield[i].draw2() : starfield[i].draw();
 }
+
+console.log(starfield);
 
 // function animate() {
 //   ctx.fillStyle = "#000";
@@ -136,30 +135,61 @@ const ctx2 = canvas2.getContext("2d");
 // Create Click Event
 
 let clickActive = false;
+const spin = randomInt(0, 1.6);
 
 // let clickT = ["click", "touchstart"];
 
 canvas2.addEventListener("click", (e) => {
   clickActive = true;
 
+  // cross
+  ctx2.save();
   ctx2.beginPath();
-  ctx2.fillStyle = `rgba(255,255,255)`;
-  ctx2.arc(e.clientX, e.clientY, 25, 0, Math.PI * 2);
+  ctx2.translate(e.clientX, e.clientY);
+  ctx2.fillStyle = "rgba(255,255,255,0.3)";
+  ctx2.strokeStyle = "#fff";
+  ctx2.strokeStyle = "rgba(255,255,255,0.1)";
+  ctx2.lineWidth = 4;
+  ctx2.rotate(`${spin}`);
+  ctx2.moveTo(0 - 75, 0);
+  ctx2.lineTo(0 + 75, 0);
+  ctx2.moveTo(0, 0 - 75);
+  ctx2.lineTo(0, 0 + 75);
+  ctx2.stroke();
+  ctx2.fill();
+  ctx2.closePath();
+  // ctx2.restore();
+
+  // star
+  ctx2.beginPath();
+  ctx2.fillStyle = "rgba(255,255,255,0.3)";
+  ctx2.strokeStyle = "rgba(255,255,255,0.1)";
+  ctx2.lineWidth = 1;
+  ctx2.moveTo(0 - 50, 0); // Start right
+  ctx2.quadraticCurveTo(0, 0, 0, 0 - 50); // To top
+  ctx2.quadraticCurveTo(0, 0, 0 + 50, 0); // To left
+  ctx2.quadraticCurveTo(0, 0, 0, 0 + 50); // To bottom
+  ctx2.quadraticCurveTo(0, 0, 0 - 50, 0); // To right
+  ctx2.stroke();
+  ctx2.fill();
+  ctx2.closePath();
+  ctx2.restore();
+
+  // circle
+  ctx2.save();
+  ctx2.beginPath();
+  ctx2.fillStyle = `rgba(255,255,255, 1)`;
+  ctx2.strokeStyle = `rgba(255,255,255, 0.1)`;
+  ctx2.lineWidth = 15;
+  ctx2.arc(e.clientX, e.clientY, `${randomInt(20, 25)}`, 0, Math.PI * 2);
   ctx2.shadowColor = "#eee";
   ctx2.shadowBlur = 50;
   ctx2.shadowOffsetX = 0;
   ctx2.shadowOffsetY = 0;
+  ctx2.stroke();
   ctx2.fill();
   ctx2.closePath();
-
-  console.log(e.clientX);
-
-  ctx2.beginPath();
-  ctx2.strokeStyle = "gray";
-  ctx2.moveTo(e.clientX, e.clientY);
-  ctx2.lineTo(0, 0);
-  ctx2.stroke();
-  ctx2.closePath();
+  ctx2.restore();
 });
 
 // track if click active, increase counter, expand radius, limit radius to percentage of width or height whichever is smaller
